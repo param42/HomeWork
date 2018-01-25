8679383d553c7fbba0faf0af37c881b45b7801af
param42 <param42@ya.ru>
2018-01-25 13:53:44 +0300

    Update main.cpp

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,1 +1,22 @@
 
+#include "stdafx.h"
+
+#include "LinkedHashTable.h"
+#include "DoubleHashTable.h"
+#include "LinearHashTable.h"
+
+#include <fstream>
+#include <chrono>
+#include <string>
+
+
+typedef std::chrono::high_resolution_clock::time_point ExecTime;
+
+#define duration(a) std::chrono::duration_cast<std::chrono::microseconds>(a).count()
+#define timeNow() std::chrono::high_resolution_clock::now()
+
+int main(int argc, char* argv[]) {
+
+  
+	system("pause");
+}
