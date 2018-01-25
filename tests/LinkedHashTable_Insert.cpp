ca5556cf838f6b4618a0ce6eec2dfe5f7085b666
param42 <param42@ya.ru>
2018-01-25 04:12:24 +0300

    Update LinkedHashTable_Insert.cpp

--- tests/LinkedHashTable_Insert.cpp
+++ tests/LinkedHashTable_Insert.cpp
@@ -2,7 +2,7 @@
 #include "catch.hpp"
 
 
-SCENARIO("When you add an item, the size increases")
+SCENARIO("When you add an item in LinkedHashTable, the size increases")
 {
     GIVEN("LinkedHashTable ")
     {
